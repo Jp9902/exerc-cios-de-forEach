1d936e29178a938e6bfb8e508530d45e594bf033
João <168004754+JnGuilherme07@users.noreply.github.com>
2024-10-21 08:05:32 -0300

    Pegando apenas os números pares com forEach

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+let numerosPares = []
+
+numeros.forEach(function (madeN) {
+    if(madeN % 2 === 0){
+
+    console.log(madeN)
+    }
+})
+console.log(numerosPares)
